a077e86c223d6dbde3cc38ccc7a25f7cb6283525
Noah Peart <noah.v.peart@gmail.com>
2024-05-12 15:00:20 -0700

    add ts-query to read/apply queries

--- ts-query.el
+++ ts-query.el
@@ -24,6 +24,8 @@
 ;;; Code:
 
 (require 'ts-util)
+(require 'ts-lang nil t)
+
 
 (defvar-local ts-query--langs nil)
 
@@ -67,6 +69,79 @@ With prefix, remove hightlights for LANG."
                                            (list 'ts-lang lang)
                                          (list 'ts-query t)))
   (setq ts-query--langs (and lang (--filter (not (eq it lang)) ts-query--langs))))
+
+
+;; -------------------------------------------------------------------
+;;; Interactive query mode
+
+(defun ts-query--read (&optional stream)
+  "Read query from STREAM.
+Point should be at start of query."
+  (let ((res (ignore-errors (read stream)))
+        sexp)
+    (and res (setq res (list res)))
+    (while (and (setq sexp (ignore-errors (read (current-buffer))))
+                (symbolp sexp)
+                (string-prefix-p "@" (symbol-name sexp)))
+      (push sexp res))
+    (nreverse res)))
+
+(defun ts-query--read-tl-query (&optional point)
+  "Read the top-level node for POINT."
+  (save-excursion
+    (goto-char (or point (point)))
+    (forward-comment (- (point-max)))
+    (let ((node (--when-let (treesit-node-at (point))
+                  (treesit-parent-while
+                   it (lambda (n) (not (string-match-p "program" (treesit-node-type n))))))))
+      (when node
+        (goto-char (treesit-node-start node))
+        (ts-query--read (current-buffer))))))
+
+;;; FIXME(5/12/24): wrap in struct
+(defvar-local ts-query--src-buf nil)
+(defvar-local ts-query--parser-list nil)
+(defvar-local ts-query--parser nil)
+
+(defun ts-query-execute-query (&optional point)
+  "Execute the query at POINT, applying highlights in source buffer."
+  (interactive (list (point)) ts-query-minor-mode)
+  (--when-let (ts-query--read-tl-query point)
+    (let ((parser ts-query--parser))
+      (with-current-buffer ts-query--src-buf
+        (ts-query-highlight-query parser it)))))
+
+(defun ts-query-clear-queries ()
+  "Remove query highlights."
+  (interactive nil ts-query-minor-mode)
+  (with-current-buffer ts-query--src-buf
+    (ts-query-remove-highlights)))
+
+(defvar-keymap ts-query-minor-mode-map
+  "C-c C-c" #'ts-query-execute-query
+  "C-c C-k" #'ts-query-clear-queries)
+
+(define-minor-mode ts-query-minor-mode
+  "Minor mode active in interactive query buffers."
+  :abbrev-table nil)
+
+;;;###autoload
+(defun ts-query ()
+  "Open interactive query buffer."
+  (interactive)
+  (let* ((src-buf (current-buffer))
+         (parsers (treesit-parser-list src-buf))
+         (buf (get-buffer-create
+               (format "*ts-query[%s]*"
+                       (file-name-base (buffer-name src-buf))))))
+    (with-current-buffer buf
+      (query-ts-mode)
+      (ts-query-minor-mode)
+      (setq ts-query--src-buf src-buf
+            ts-query--parser-list parsers
+            ts-query--parser (car parsers))
+      (pop-to-buffer buf))))
+
 
 (provide 'ts-query)
 ;; Local Variables:
